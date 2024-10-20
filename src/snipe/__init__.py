6a50cae4b023297f135869fe396fffba582348f4
Mohamed Abuelanin <mabuelanin@gmail.com>
2024-10-20 10:30:18 -0700

    make-a-release

--- src/snipe/__init__.py
+++ src/snipe/__init__.py
@@ -2,3 +2,5 @@ from snipe.api.snipe_sig import SnipeSig
 from snipe.api.enums import SigType
 from snipe.api.multisig_reference_QC import MultiSigReferenceQC
 from snipe.api.reference_QC import ReferenceQC
+
+__version__ = '0.1.0'
